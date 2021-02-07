f04bd96230c5a04ba506cad1e492846114879857
timolor <timolordave@gmail.com>
2021-02-07 02:36:22 +0100

    changed  endpoint to airand domain

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -4,9 +4,9 @@ import moment from 'moment';
 const ls = require('local-storage');
 
 export const baseUrl = () => {
-  return 'https://cors-anywhere.herokuapp.com/https://airandapi.azurewebsites.net/';
+  // return 'https://cors-anywhere.herokuapp.com/https://airandapi.azurewebsites.net/';
   //return 'https://airandapi.azurewebsites.net/';
-  // return 'https://api.airand.net/';
+  return 'https://api.airand.net/';
   // return 'http://localhost:5000/';
 };
 
